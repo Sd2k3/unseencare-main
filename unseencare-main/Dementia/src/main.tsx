582092421b8f10ce1f333f8e0869334486623bb8
Sd2k3 <swarajit19082003@gmail.com>
2025-06-28 23:51:30 +0530

    removed clerk

--- unseencare-main/Dementia/src/main.tsx
+++ unseencare-main/Dementia/src/main.tsx
@@ -2,22 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+
 import { AuthProvider } from './context/AuthContext.tsx'
 
 
-const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!publishableKey || publishableKey === '') {
-  throw new Error("Missing Publishable Key")
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-    <ClerkProvider publishableKey={publishableKey} afterSignOutUrl="/">
       <App />
-    </ClerkProvider>
+   
     </AuthProvider>
   </React.StrictMode>,
 )
